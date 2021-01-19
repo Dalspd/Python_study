5c13b21627864d0a7e0c56c7d8894fe34daa57bf
sideffects <sideffects>
2021-01-19 11:34:30 +0800

    first python learning

--- 1.py
+++ 1.py
@@ -1,1 +1,17 @@
+# Welcome
 print("welcome to this repo.")
+
+# A test for pycharm[x.print==>print(x)]
+x = 3
+print(x)  # x.print
+
+# 换行符
+one = 1
+two = 2
+three = 3
+total = one + \
+        two \
+        + three
+
+fruits = ['apple', 'banana',
+          'orange']  # [],{},()不需要换行符
